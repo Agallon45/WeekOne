8fe503d6dae9e8908c36d2066f199025d2eef871
Agallon45 <viktor.eklund45@gmail.com>
2021-01-19 10:25:08 +0100

    Initial commit

---
+++ DayTwo/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace DayTwo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ulong myNumber = 0000_0002;
+            Console.WriteLine(myNumber);
+        }
+    }
+}
